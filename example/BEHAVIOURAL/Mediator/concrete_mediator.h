22c1c833168b58261534e96e2b6f4516017b7790
rainlong <yulong388@163.com>
2018-09-15 10:26:34 +0800

    Update concrete_mediator.h

--- example/BEHAVIOURAL/Mediator/concrete_mediator.h
+++ example/BEHAVIOURAL/Mediator/concrete_mediator.h
@@ -1,4 +1,4 @@
-/* 
+/* 中介者是上层，也是中枢，他掌控全局，所有人都来找他
  */
 
 #pragma once
@@ -12,6 +12,8 @@ public:
     ConcreteMediator( Colleague* clgA, Colleague* clgB ) { IntroColleague(clgA, clgB);}
     virtual ~ConcreteMediator() {}
     
+    
+    // 中介者需要知道所有负责组件的名字和职责行为
     inline void setColleagueA( Colleague* clgA ) { clgA_ = clgA; }
     inline void setColleagueB( Colleague* clgB ) { clgB_ = clgB; }
     inline Colleague* getColleagueA() { return clgA_; }
@@ -23,6 +25,7 @@ public:
         clgB_ = clgB;
     }
     
+    // 中介者负责安排对应组件做出响应
     virtual void notifyColleagueA(Colleague::State& sdt);
     virtual void notifyColleagueB(Colleague::State& sdt);
     
